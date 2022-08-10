4c1838a030d4caaf8659f85d7b54a0ace22273d2
Chris Argyros <cargyros430@gmail.com>
2022-08-09 22:49:29 -0500

    variables, selectors, and event listeners

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let Operand1 = ''
+let Operand2 = ''
+let currentOperation = null
+let ResetScreen = false
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const pointButton = document.getElementById('pointBtn')
+const lastOperationScreen = document.getElementById('lastOperationScreen')
+const currentOperationScreen = document.getElementById('currentOperationScreen')
+
+window.addEventListener('keydown', handleKeyboardInput)
+equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+deleteButton.addEventListener('click', deleteNumber)
+pointButton.addEventListener('click', appendPoint)
+
+
 function add(a, b) {
     return a + b
   }
